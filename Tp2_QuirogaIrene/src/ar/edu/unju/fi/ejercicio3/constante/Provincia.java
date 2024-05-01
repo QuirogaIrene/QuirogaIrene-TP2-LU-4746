1c9fb1b6fd5f07208c7fc441fce062762b7ee633
Quiroga Irene <ireneq142@gmail.com>
2024-04-30 21:27:08 -0300

    Agregado del punto 3 y su respectiva rama

---
+++ Tp2_QuirogaIrene/src/ar/edu/unju/fi/ejercicio3/constante/Provincia.java
@@ -0,0 +1,33 @@
+package ar.edu.unju.fi.ejercicio3.constante;
+
+public enum Provincia {
+		//nombre de la provincia (poblacion,superficie en k^2)
+	 	JUJUY(776086, 53219),
+	    SALTA(1333365, 155488),
+	    TUCUMAN(1694659, 22524),
+	    CATAMARCA(367820, 102602),
+	    LA_RIOJA(333642, 89680),
+	    SANTIAGO_DEL_ESTERO(874006, 136351);
+		
+		private final double cantidadPoblacion;
+		private  final double superficie;
+		
+		private Provincia(double cantidadPoblacion, double superficie) {
+			this.cantidadPoblacion = cantidadPoblacion;
+			this.superficie = superficie;
+		}
+
+		public double getCantidadPoblacion() {
+			return cantidadPoblacion;
+		}
+
+
+		public double getSuperficie() {
+			return superficie;
+		}
+
+		 public double calcularDensidadPoblacional() {
+		        return (double) cantidadPoblacion / superficie;
+		    }
+		
+}
---
+++ Tp2_QuirogaIrene/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio3.main;
+
+import ar.edu.unju.fi.ejercicio3.constante.Provincia;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Provincia[] provincias = Provincia.values();
+
+        //Muestra la informacion de cada provincia
+        for (Provincia provincia : provincias) {
+            System.out.println("Provincia: " + provincia);
+            System.out.println("Poblacion: " + provincia.getCantidadPoblacion());
+            System.out.println("Superficie: " + provincia.getSuperficie() + " km^2");
+            System.out.println("Densidad Poblacional: " + provincia.calcularDensidadPoblacional() + " hab/km^2");
+            System.out.println(); // Salto de línea para separar a cada  una de las provincias.
+        }
+	}
+
+}
